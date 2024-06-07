610767ee3e970f5bfe7f4afd86c7adbececad14d
govindhamma82 <govindhamma82@gmail.com>
2024-06-07 16:44:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling to section
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
